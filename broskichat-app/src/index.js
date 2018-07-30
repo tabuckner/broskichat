0131562d360e1ca9dda8dd92b502832acdff6806
Taylor Buckner <32392635+tabuckner@users.noreply.github.com>
2018-07-30 18:52:25 -0500

    Quick fixes (#5)

--- broskichat-app/src/index.js
+++ broskichat-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bulma/css/bulma.css' // CSS Framework
+import './index.css'; // Main App Styles
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
