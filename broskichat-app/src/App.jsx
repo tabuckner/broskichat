f944d3ff6b7058d36a83875cd85e76b04a2c216c
Andrew Lee <38574392+akeon-lee@users.noreply.github.com>
2018-08-02 17:03:21 -0700

    Integrated header (#6)
    * Played around with React, Created Componenets folder with a header.js
    
    * Integrated Bulma and started header-nav
    
    * Header files setup
    
    * Header.js skeleton completed
    
    * Implemented react tests and --dev add react-test-renderer
    
    * Added a quick note about menuItems
    
    * Changed Header.js spacing, deleted custom css in App.css, Removed extra spaces in App.jsx

--- broskichat-app/src/App.jsx
+++ broskichat-app/src/App.jsx
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+// Imports of ./Components
+import Header from './Components/Partials/Header';
+
 class App extends Component {
   render() {
 
     return (
 
       <div>
+        <Header />
 
         <div className="container App">
-          <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          {/* Main Container Area */}
         </div>
-        
+
       </div>
     );
   }
--- broskichat-app/src/App.test.js
+++ broskichat-app/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Components/Partials/Header';
+
+import TestRenderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('<Header />', () => {
+  it('Header logoTitle === \'Broski Test\'', () => {
+    // Create a renderer instance
+    const header = TestRenderer.create(<Header />);
+    // instance root
+    const root = header.root;
+
+    // Expect <a className="navbar-item is-size-5"> to equal 'Broski Test'
+    expect(root.findByProps({className: "navbar-item is-size-5"}).children).toEqual([ 'Broski Test' ]);
+  });
 });
