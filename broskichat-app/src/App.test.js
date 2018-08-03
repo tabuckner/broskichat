abcf16924972900db6d193f92c1f0fe794c4779b
Andrew Lee <akeon.lee@gmail.com>
2018-08-02 21:59:07 -0700

    Hamburger and dropdown menu configured

--- broskichat-app/src/App.test.js
+++ broskichat-app/src/App.test.js
@@ -12,13 +12,13 @@ it('renders without crashing', () => {
 });
 
 describe('<Header />', () => {
-  it('Header logoTitle === \'Broski Test\'', () => {
+  it('Header this.title === \'BroskiChat\'', () => {
     // Create a renderer instance
     const header = TestRenderer.create(<Header />);
     // instance root
     const root = header.root;
 
     // Expect <a className="navbar-item is-size-5"> to equal 'Broski Test'
-    expect(root.findByProps({className: "navbar-item is-size-5"}).children).toEqual([ 'Broski Test' ]);
+    expect(root.findByProps({className: "navbar-item is-size-5"}).children).toEqual([ 'BroskiChat' ]);
   });
 });
--- broskichat-app/src/Components/Partials/Header.jsx
+++ broskichat-app/src/Components/Partials/Header.jsx
@@ -3,31 +3,95 @@ import React from 'react';
 // import logo from '../../logo.svg'; // Use logo later
 
 class Header extends React.Component {
-  // constructor(props) { // Kept for when passing in props
-  //     super(props);
-  // }
+  constructor(props) { // Kept for when passing in props
+      super(props);
+
+      /* Functions */
+      // When hamburger is clicked
+      this.isOpen = false;
+      this.onClickHamburger = () => {
+        // Grab elements `menu`, `burger`
+        const menu = this.refs.navMenu,
+              burger = this.refs.hamburger;
+
+        if(!this.isOpen) {
+          // Add active class
+          menu.classList.add('is-active');
+          burger.classList.add('is-active');
+
+        } else {
+          // Remove active class
+          menu.classList.remove('is-active');
+          burger.classList.remove('is-active');
+
+        }
+        // Set isOpen to opposite
+        this.isOpen = !this.isOpen;
+        
+      }
+      
+      /* Header Items */
+      this.title = "BroskiChat";
+
+      // Nav menu items
+      this.menuItems = [
+        { name:"Chat", path: "", 
+          children: {
+            name1:"Placeholder1", path1: "",
+            name2:"Placeholder2", path2: ""
+          } }, 
+        { name:"About", path: "" }, 
+        { name:"Contact", path: "" } ].map((item, index) => {
+        
+        // If item have children
+        if(item.children) {
+          return  <div className="navbar-item has-dropdown is-hoverable" key={index}> 
+                    {/* `has-text-light` can be removed when Bulmaswatch */}
+                    <a className="navbar-link has-text-light">
+                      {item.name}
+                    </a>
+                    {/* `navbar` and `is-dark` can be removed when Bulmswatch */}
+                    <div className="navbar navbar-dropdown has-text-light is-dark">
+                      <a className="navbar-item has-text-light" href={item.children.path1}>
+                        {item.children.name1}
+                      </a>
+                      <a className="navbar-item has-text-light" href={item.children.path2}>
+                        {item.children.name2}
+                      </a>
+                      <hr className="navbar-divider" />
+                      <div className="navbar-item has-text-light">
+                        Version 0.0
+                      </div>
+                    </div>
+                  </div>;
+        } else {
+          // Return each element w/ key
+          return <a className="navbar-item has-text-light" href={item.path} key={index}> {item.name} </a>;
+        }
+      });  
+  }
   
   render() {
-    const logoTitle = "Broski Test"
-    // Change array to update menu items. map() each element.
-    const menuItems = [ "Chat", "About", "Contact" ].map((item, index) => {
-      // Return each element w/ key
-      return <a className="navbar-item has-text-light" key={index}> {item} </a>;
-    });
-
     return (
-      <nav className="navbar navbar-menu is-dark">
+      <nav className="navbar is-black">
         <div className="navbar-brand">
           <a className="navbar-item is-size-5">
 
-            { logoTitle }
+            { this.title }
 
           </a>
+
+          <div className="navbar-burger" ref="hamburger" onClick={this.onClickHamburger}>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
         </div>
-        <div className="navbar-menu">
+        {/* `navbar` and `is-dark` can be removed when Bulmswatch */}
+        <div className="navbar navbar-menu is-dark" ref="navMenu">
           <div className="navbar-start">
             
-            { menuItems }
+            { this.menuItems }
 
           </div>
           <div className="navbar-end">
