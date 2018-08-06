5d05786df3d65ba7c61250f41907969e9bf00f55
Taylor Buckner <tbuckner@theappraisallane.com>
2018-08-06 16:54:44 -0500

    Added Enzyme for shallow and overall better testing.

--- broskichat-app/src/App.test.js
+++ broskichat-app/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('shallow renders without crashing', () => {
+    shallow(<App />);
+  });
 });
--- broskichat-app/src/Components/Home/Marketing/Marketing.test.js
+++ broskichat-app/src/Components/Home/Marketing/Marketing.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
 import Marketing from './Marketing';
 import Point from './Point/Point';
+import { mount } from 'enzyme';
 
 
 describe('<Marketing />', () => {
   
-  describe('Should create a number of points', () => {
+  it('Should create a number of points', () => {
     const mockProps = [
       { img: null, title: 'Come Join', text: 'Join our community and start chatting today.' },
       { img: null, title: 'Realtime', text: 'Chat with others in real time about whatever you\'d like.' }
     ];
-    const marketing = TestRenderer.create(<Marketing content={mockProps} />);
-    const testInstance = marketing.root;
-    const points = testInstance.findByType(Point);
-    console.log(marketing.toJSON());
+    const marketing = mount(<Marketing content={mockProps} />);
+    expect(marketing.find(Point)).toHaveLength(2);
 
-    expect(point).toBe(2);
+
   });
 
 });
