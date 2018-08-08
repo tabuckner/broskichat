e84729466ac63eeb06f81e73c24d4246c2a8b1c1
Andrew Lee <38574392+akeon-lee@users.noreply.github.com>
2018-08-08 12:00:38 -0700

    Integrated header (#16)
    * Played around with React, Created Componenets folder with a header.js
    
    * Integrated Bulma and started header-nav
    
    * Header files setup
    
    * Header.js skeleton completed
    
    * Implemented react tests and --dev add react-test-renderer
    
    * Added a quick note about menuItems
    
    * Changed Header.js spacing, deleted custom css in App.css, Removed extra spaces in App.jsx
    
    * Hamburger and dropdown menu configured
    
    * Added quick notes to remove className after Bulmaswatch
    
    * Added and removed some comments
    
    * Fixed the Header.test.js file

--- broskichat-app/src/Components/Partials/Header.test.js
+++ broskichat-app/src/Components/Partials/Header.test.js
@@ -3,13 +3,13 @@ import TestRenderer from 'react-test-renderer';
 import Header from './Header';
 
 describe('<Header />', () => {
-  it('Header logoTitle === \'Broski Test\'', () => {
+  it('Header this.title === \'BroskiChat\'', () => {
     // Create a renderer instance
     const header = TestRenderer.create(<Header />);
     // instance root
     const root = header.root;
 
-    // Expect <a className="navbar-item is-size-5"> to equal 'Broski Test'
-    expect(root.findByProps({className: "navbar-item is-size-5"}).children).toEqual([ 'Broski Test' ]);
+    // Expect <a className="navbar-item is-size-5"> to equal 'BroskiChat'
+    expect(root.findByProps({className: "navbar-item is-size-5"}).children).toEqual([ 'BroskiChat' ]);
   });
 });
