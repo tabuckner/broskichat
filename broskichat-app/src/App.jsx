cdda8367ae576837cd82f5e3f87355f204883448
Matthew Kim <34763159+mattkim8@users.noreply.github.com>
2018-08-14 15:28:05 -0400

    Update App.jsx

--- broskichat-app/src/App.jsx
+++ broskichat-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
 // Imports of ./Components
 import Header from './Components/Partials/Header';
 import Home from './Components/Home/Home';
@@ -9,17 +8,13 @@ import Footer from './Components/Partials/Footer';
 
 class App extends Component {
   render() {
-
     return (
-
       <React.Fragment>
         <Header />
-
         <div className="App">
           <Home />
           <About />
         </div>
-
         <Footer />
       </React.Fragment>
     );
