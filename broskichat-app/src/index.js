8801e307189924e84bb893d8bb64a6a7aa572a07
Taylor Buckner <tbuckner@theappraisallane.com>
2018-08-20 18:53:44 -0500

    Added a simple 404 page

--- broskichat-app/src/index.js
+++ broskichat-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/slate/bulmaswatch.min.css'; // CSS Framework
 import './index.css'; // Main App Styles
+import './Components/NotFound/NotFound.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
