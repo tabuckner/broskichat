70c41882371c1d6d03d395bef3eb57b73b1521f3
Taylor Buckner <32392635+tabuckner@users.noreply.github.com>
2018-08-20 19:18:59 -0500

    Added a simple 404 page (#26)

--- broskichat-app/src/App.jsx
+++ broskichat-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Switch from 'react-router-dom/Switch';
 
 import './App.css';
 // Imports of ./Components
@@ -8,6 +9,7 @@ import Footer from './Components/Partials/Footer';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Register from './Components/Users/Register/Register';
+import NotFound from './Components/NotFound/NotFound';
 
 class App extends Component {
   render() {
@@ -16,9 +18,12 @@ class App extends Component {
         <Header />
         <Router>
           <div className="App">
-            <Route exact path={"/"} component={Home} />
-            <Route path={"/about"} component={About} />
-            <Route path={"/register"} component={Register} />
+            <Switch>
+              <Route exact path={"/"} component={Home} />
+              <Route path={"/about"} component={About} />
+              <Route path={"/register"} component={Register} />
+              <Route component={NotFound}/>
+            </Switch>
           </div>
         </Router>
         <Footer />
