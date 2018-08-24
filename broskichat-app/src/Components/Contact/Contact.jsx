e780ac7d64178650513a4c213e1cbdd629815890
Matthew Kim <34763159+mattkim8@users.noreply.github.com>
2018-08-23 20:10:27 -0400

    Update Contact.jsx

--- broskichat-app/src/Components/Contact/Contact.jsx
+++ broskichat-app/src/Components/Contact/Contact.jsx
@@ -6,80 +6,80 @@ class Contact extends React.Component {
     return (
       <div>
 
-        <div class="tile is-ancestor">
-          <div class="tile is-vertical is-8">
-            <div class="tile is-parent">
-              <article class="tile is-child notification is-light">
-                <p class="title">
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical is-8">
+            <div className="tile is-parent">
+              <article className="tile is-child notification is-light">
+                <p className="title">
                   Contact Us
                 </p>
-                <p class="subtitle">
+                <p className="subtitle">
                   Ipsum Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam doloribus voluptas ad quasi dignissimos.
                 </p>
-                <div class="content">
+                <div className="content">
                   {/* --Form--*/}
-                  <div class="field">
-                    <label class="label">
+                  <div className="field">
+                    <label className="label">
                       Your Name(required)
                     </label>
-                    <div class="control">
-                      <input class="input" type="text" placeholder="e.g Alex Smith" />
+                    <div className="control">
+                      <input className="input" type="text" placeholder="e.g Alex Smith" />
                     </div>
                   </div>
-                  <div class="field">
-                    <label class="label">
+                  <div className="field">
+                    <label className="label">
                       Your Email(required)
                     </label>
-                    <div class="control">
-                      <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                    <div className="control">
+                      <input className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
                     </div>
                   </div>
-                  <div class="field">
-                    <label class="label">
+                  <div className="field">
+                    <label className="label">
                       Subject
                     </label>
-                    <div class="control">
-                      <input class="input" />
+                    <div className="control">
+                      <input className="input" />
                     </div>
                   </div>
-                  <div class="field">
-                    <label class="label">
+                  <div className="field">
+                    <label className="label">
                       Your Message
                     </label>
-                    <div class="control">
-                      <textarea class="textarea" rows="10"></textarea>
+                    <div className="control">
+                      <textarea className="textarea" rows="10"></textarea>
                     </div>
                   </div>
                   {/*--Form--*/}
 
                 </div>
-                <div class="control">
-                  <button class="button is-dark">Submit</button>
+                <div className="control">
+                  <button className="button is-dark">Submit</button>
                 </div>
               </article>
             </div>
           </div>
-          <div class="tile is-parent">
-            <article class="tile is-child notification is-black">
-              <div class="content">
-                <p class="title">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-black">
+              <div className="content">
+                <p className="title">
                   Email
                 </p>
-                <p class="subtitle">
+                <p className="subtitle">
                   blahblah@blah.com
                 </p>
                 <hr></hr>
-                <p class="title">
+                <p className="title">
                   Phone Number
                 </p>
-                <p class="subtitle">
+                <p className="subtitle">
                   123-456-7890
                 </p>
                 <hr></hr>
-                <p class="title">
+                <p className="title">
                   Social Media/Github
                 </p>
-                <p class="subtitle">
+                <p className="subtitle">
                   blahblah@blah.com
                 </p>
                 <hr></hr>
