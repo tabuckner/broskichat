9906b442ba6747e762bea9a6bb1349a3b4d4d8f1
Andrew Lee <akeon.lee@gmail.com>
2018-08-29 16:57:14 -0700

    Setup basic chat message box

--- broskichat-app/src/Components/Chat/Form/Form.jsx
+++ broskichat-app/src/Components/Chat/Form/Form.jsx
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
 
 class Form extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
 			<form action="#">
+				<section className="toolbox">
+					<div className="dropdown is-hoverable is-up">
+						<div className="dropdown-trigger">
+							<button type="button" className="button is-warning">
+								<span><FA icon="smile" /></span>
+							</button>
+						</div>
+						<div className="dropdown-menu">
+							<div className="dropdown-content has-background-black">
+								<div className="dropdown-item">
+									<div className="columns is-mobile">
+										<div className="column">
+											<a className="navbar-item has-text-white"><FA icon="smile" /></a>
+											<a className="navbar-item has-text-white"><FA icon="toolbox" /></a>
+											<a className="navbar-item has-text-white"><FA icon="share" /></a>
+										</div>
+										<div className="column">
+											<a className="navbar-item has-text-white"><FA icon="smile" /></a>
+											<a className="navbar-item has-text-white"><FA icon="toolbox" /></a>
+											<a className="navbar-item has-text-white"><FA icon="share" /></a>
+										</div>
+										<div className="column">
+											<a className="navbar-item has-text-white"><FA icon="smile" /></a>
+											<a className="navbar-item has-text-white"><FA icon="toolbox" /></a>
+											<a className="navbar-item has-text-white"><FA icon="share" /></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 				<div className="field">
-					<label className="label">Message</label>
-					<p className="control">
-						<input
+					<p className="control has-icons-left has-icons-right">
+						<textarea
 							value={this.props.value}
 							onChange={this.props.onChange}
-							className="input" type="text" name="message" placeholder="Enter Message" />
+							className="textarea" type="text" name="message" placeholder="Start Chatting By Typing A Message." />
 					</p>
+					<button onClick={this.props.onClick} className="button is-info is-medium is-fullwidth">Send</button>
 				</div>
-				<button onClick={this.props.onClick} className="button is-info">Send</button>
 			</form>
 		) 
 	}	
